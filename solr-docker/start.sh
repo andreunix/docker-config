daa7dd95a64eea0e7a01bf1fe2b5aeec4690f4f3
andreunix <andreunix@protonmail.com>
2024-02-07 12:32:37 -0400

    add config

---
+++ solr-docker/start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Espera o ZooKeeper ficar pronto
+while ! nc -z zoo1 2181; do   
+  sleep 0.1
+done
+folder_name="myconfig"
+# Verifica se a configuração já existe no ZooKeeper
+if bin/solr zk ls /configs/$folder_name -z zoo1:2181,zoo2:2181,zoo3:2181 | grep -q $folder_name; then
+    echo "A configuração '$folder_name' já existe no ZooKeeper. Pulando a etapa de envio."
+else
+    echo "A configuração '$folder_name' não existe. Enviando para o ZooKeeper..."
+    bin/solr zk upconfig -n $folder_name -d /opt/solr/server/solr/configsets/myconfig -z zoo1:2181,zoo2:2181,zoo3:2181
+fi
+
+# Mantém o contêiner em execução
+exec solr-foreground
